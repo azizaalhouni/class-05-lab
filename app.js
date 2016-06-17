f2e010cae392102f99385aae2783bedbcd163aaf
aziza alhouni <azizaalhouni@yahoo.com>
2016-06-17 15:34:48 -0700

    solve Problem4

--- app.js
+++ app.js
@@ -79,12 +79,16 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 testArray = [2,3,4];
 function sumArray(testArray){
-
+  var sum1 = sum(testArray[0], testArray[1])[0];
+  console.log(sum1);
+  var finalSum = sum(sum1, testArray[2])[0];
+  return [finalSum, testArray + ' was passed in as an array of numbers, and ' + finalSum + ' is their sum.'];
+//  console.log(sumArray);
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle, synchronize the code between both GitHubs and both laptops, and swap Driver/Navigator roles. Don't forget to create a new branch for your work on the next question!
 
